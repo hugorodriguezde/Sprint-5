693f24f412a1167fd1306c372874e5c33106f5db
Hugo Rodríguez <hugorodriguezde@gmail.com>
2024-02-05 12:55:49 +0100

    solved lvl 1 and 2

--- src/app/Home/escena/escena.component.ts
+++ src/app/Home/escena/escena.component.ts
@@ -2,38 +2,39 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { iStep } from '../../Interface/iStep.interface';
 
-
-
 @Component({
   selector: 'app-escena',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './escena.component.html',
-  styleUrl: './escena.component.scss'
-})
-export class EscenaComponent {
+  styleUrl: './escena.component.scss',
+  })
+
+  export class EscenaComponent {
 
   @Input()
   public steps: iStep[] = [];
 
-  public currentStep = 0;
+  public actualStep = 0;
 
   last() {
-    if (this.currentStep > 0) {
-      this.currentStep--;
+    if (this.actualStep > 0) {
+      this.actualStep--;
     } else {
-      this.currentStep = this.steps.length - 1;
+      this.actualStep = this.steps.length - 1;
     }
   }
 
-  next() {
-    if (this.currentStep < this.steps.length - 1) {
-      this.currentStep++;
+  next():void {
+    if (this.actualStep < this.steps.length - 1) {
+      this.actualStep++;
     } else {
-      this.currentStep = 0
+      this.actualStep = 0
     }
   }
+
   goToStep(index: number): void {
-    this.currentStep = index;
+    this.actualStep = index;
   }
+
 }
--- src/app/Services/steps.service.ts
+++ src/app/Services/steps.service.ts
@@ -11,16 +11,16 @@ export class StepsService {
       title: "Dedica moltes hores",
       description: "Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.",
       img: "./assets/img/time_managment.svg",
-      bgcolor: "#3BB8C8",
+      bgcolor: "#17888a",
     }, {
       title: "Programa projectes propis",
       description: "Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.",
       img: "./assets/img/programming.svg",
-      bgcolor: "#E0E2E3",
+      bgcolor: "#cfd2d4",
     }, {
       title: "Procura descansar",
       description: "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
       img: "./assets/img/meditation.svg",
-      bgcolor: "#F8E55D",
+      bgcolor: "#f2e377",
     }];
 }
